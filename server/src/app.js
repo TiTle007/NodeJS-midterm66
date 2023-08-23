539e4bca69713baf2f64ce86990023e3d9281b49
TiTle007 <tlemoil555@gmail.com>
2023-08-23 12:38:34 +0700

    midterm66

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,23 @@
 let express = require('express')
 let bodyParser = require('body-parser')
-const {sequelize} = require('./models')
-
-const config = require('./config/config')
 
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extends:true}))
+app.use(bodyParser.urlencoded({extended:true}))
 
-require('./rout')(app)
+require('./route')(app)
 
-app.get('/status', function (req, res ){
-    res.send('Hello nodejs server 12345678')
+app.get('/status', function (req,res){
+    res.send('Hello nodejs server belong to nattapon')
 })
 
-app.get('/hello/:person', function (req,res){
-    console.log('hello - '+ req.params.person);
-    res.send('sey hello with '+req.params.person)
-})
+app.get('/hello/:name', function (req,res) {
+    console.log('hello - ' + req.params.name)
+    res.send('sey hello with ' + req.params.name) 
+   })
 
-let port = process.env.PORT || config.port
-
-sequelize.sync({force: false}).then(() => { 
-    app.listen(port, function () {
-        console.log('Server running on ' + port) 
-    })
+let port = 8081
+app.listen(port, function(){
+    console.log('server running on ' + port)
 })
